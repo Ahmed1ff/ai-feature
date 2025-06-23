3b6142266abafea6466de6b28c66883b4481e80e
a7meddidy <af7974943@gmail.com>
2025-06-23 22:53:33 +0300

    first commit

--- index.py
+++ index.py
@@ -4,8 +4,12 @@ from src.search_by_image.search import app as search_app  # removed lifespan
 main_app = FastAPI()
 
 # Mount the inner app under a prefix
+main_app.mount("/search", search_app)
+
+# Root-level route for testing
 @main_app.get("/")
-def read_root():
-    return {"message": "Hello from Azure!"}
+def root():
+    return {"message": "Main App is running"}
+
 # Command to run:
 # uvicorn index:main_app --host 0.0.0.0 --port 8000
